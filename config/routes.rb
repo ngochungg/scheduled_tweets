a579528f9c7053c684bffafd167caba747243463
Ngoc Hung <ngochungg16@gmail.com>
2023-06-03 01:17:01 +0700

    modify and add reset password by mail

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,21 @@ Rails.application.routes.draw do
   #GET /about
   get "about", to: "about#index"
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_reset#new"
+  post "password/reset", to: "password_reset#create"
+  get "password/reset/edit", to: "password_reset#edit"
+  patch "password/reset/edit", to: "password_reset#update"
 
   root to: "main#index"
 end
