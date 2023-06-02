b7051d015e50252538bb838dadd771ecedaed89d
Ngoc Hung <ngochungg16@gmail.com>
2023-06-02 22:51:16 +0700

    Add registration and logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   #GET /about
   get "about", to: "about#index"
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
+
   root to: "main#index"
 end
